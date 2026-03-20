d34ebbeebd3f1f87485e8951cc97bef5adef555e
Brittani Lovelace <blovelace84@gmail.com>
2026-03-20 18:47:59 -0400

    Update list

---
+++ UpdateList.js
@@ -0,0 +1,8 @@
+console.log("Here is the Updated List for Julius Caesar's party: ");
+
+let myList = ["Nero", "Caligula", "Claudius", "Tiberius"];
+
+myList.push("Augustus");
+myList.unshift("Julius Caesar");
+
+console.log(myList);
